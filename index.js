c1aa2ddc2fb30317259286307b7ce0d91d0b5413
rdschennai <156580005+rdschennai@users.noreply.github.com>
2025-06-15 00:23:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -92,3 +92,7 @@ app.get('/', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const cors = require('cors');
+app.use(cors());
+const cors = require('cors');
+app.use(cors());
